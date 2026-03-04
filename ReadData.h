5bdd3afb8064bf3a47b24af29db15ad73891150d
Gabriele Garbin <garbin.gabbo@gmail.com>
2026-03-04 23:51:04 +0100

    Aggiungere i file di progetto.

---
+++ ReadData.h
@@ -0,0 +1,11 @@
+#include "SharedFileOut.h"
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+
+
+int InitSM();
+void DismissSM();
+void ReadPhysics(SPageFilePhysics * outData);
+void ReadGraphics(SPageFileGraphic * outData);
+void ReadStatic(SPageFileStatic * outData);
